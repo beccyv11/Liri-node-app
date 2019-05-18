c467114a9d98d370080471f59471e01ed8ef9ca5
The Becs <beccyvalentine@gmail.com>
2019-05-18 13:46:47 -0700

    changed to the script so that they are arrays rather than console.log statements, as well as an added log.txt file.

--- liri.js
+++ liri.js
@@ -12,54 +12,43 @@ var moment = require('moment');
 
 var fs = require("fs");
 
-var command = process.argv[2]
-
 var spotify = new Spotify(keys.spotify);
-var songName = process.argv.slice(3).join(" ");
-if (songName === ""){
-    var songName = "The Sign"
-};
 
-switch(command) {
-    case "spotify-this-song":
-      runSpotify();
-      break;
-    case `movie-this`:
-      runOmdb();
-      break;
-    case `concert-this`:
-      runBand();
-      break;
-    case 'do-what-it-says':
-      runText();
-      break;
-  }
+var divider = "\n------------------------------------------------------------\n\n";
 
-function runSpotify () {
+
+
+var runSpotify = function(songName) {
+    if (songName === ""){
+        var songName = "The Sign"
+    };
 spotify
 .search({
  type: 'track' , 
  query: songName
  })
 .then(function(response) {
-    console.log(response.tracks.items[0].artists[0].name);
-    console.log(response.tracks.items[0].preview_url);
-    console.log(response.tracks.items[0].album.name);
-//   console.log(response.tracks.items);
-})
-.catch(function(err) {
-  console.log(err);
-});
-}
+    var jsonData = response.tracks.items[0];
+    var showData = [
+        "Artist: " + jsonData.artists[0].name,
+        "Song Name: " + songName,
+        "Preview Link" + jsonData.preview_url,
+        "Album Name: " + jsonData.album.name, 
+      ].join("\n\n");
 
+      fs.appendFile("log.txt", showData + divider, function(err) {
+        if (err) throw err;
+        console.log(showData);
+      })
+    });
+};
 
-function runOmdb () {
-
-var movieName = process.argv.slice(3).join(" ");
+var runOmdb = function(movieName) {
 
 if (movieName === ""){
     var movieName = "Mr. Nobody"
 };
+
 
 // Then run a request with axios to the OMDB API with the movie specified
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
@@ -67,51 +56,88 @@ var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey
 axios
     .get(queryUrl).then(
     function (response){
-        console.log(response.data.Title);
-        console.log(response.data.Year);
-        console.log(response.data.imdbRating);
-        console.log(response.data.Ratings[1].Value);
-        console.log(response.data.Country);
-        console.log(response.data.Language);
-        console.log(response.data.Plot);
-        console.log(response.data.Actors);
+        var movieData=response.data;
+        var showData = [
+            "Title: " + movieData.Title,
+            "Release Year " + movieData.Year,
+            "imdb Rating" + movieData.imdbRating,
+            "Rotten Tomato Rating: " + movieData.Ratings[1].Value, 
+            "Country: " + movieData.Country,
+            "Language:" + movieData.Language,
+            "Plot:" + movieData.Plot, 
+            "Actors: " + movieData.Actors
+          ].join("\n\n");
+          fs.appendFile("log.txt", showData + divider, function(err) {
+            if (err) throw err;
+            console.log(showData);
+        });
+       
 });
 }
 
 
 
-function runBand () { 
-
-var artist = process.argv.slice(3).join(" ");
+var runBand = function(artist) { 
     
 var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     
 axios 
     .get(queryUrl).then(
     function (response) {
-        console.log(response.data[1].venue.name);
-        console.log(response.data[1].venue.city);
-        console.log(response.data[1].venue.country);
-        console.log(moment(response.data[1].datetime).format('MM/DD/YYYY'));
+        var concertData=response.data[1];
+        var showData = [
+            "Venue: " + concertData.venue.name,
+            "City: " + concertData.venue.city,
+            "Country:" + concertData.venue.country,
+            "Date: " + (moment(concertData.datetime).format('MM/DD/YYYY'))
+          ].join("\n\n");
+          fs.appendFile("log.txt", showData + divider, function(err) {
+            if (err) throw err;
+            console.log(showData);
+        });
+    
 });
 
 }
 
-function runText() { 
+var runText = function() { 
 
-fs.readFile("random.txt", "utf8", function(err, data) {
-  if (err) {
-    return console.log(err);
-  }
-
+fs.readFile("random.txt", "utf8", function(error, data) {
+  console.log(data);
   // Break the string down by comma separation and store the contents into the output array.
-  var output = data.split(",");
+  var dataArr = data.split(",");
 
   // Loop Through the newly created output array
-  for (var i = 0; i < output.length; i++) {
-
-    // Print each element (item) of the array/
-    console.log(output[i]);
+  if (dataArr.length ===2 ){
+      pick(dataArr[0], dataArr[1]);
+  }
+  else if (dataArr.length === 1){
+      pick(dataArr[0]);
   }
 });
 };
+
+var pick = function (command, fucntionData){
+    switch(command) {
+        case "spotify-this-song":
+          runSpotify(fucntionData);
+          break;
+        case `movie-this`:
+          runOmdb(fucntionData);
+          break;
+        case `concert-this`:
+          runBand(fucntionData);
+          break;
+        case 'do-what-it-says':
+          runText();
+          break;
+        default:
+            console.log("Liri doesn't know that");
+      };
+    };
+
+var start = function (argOne, argTwo) {
+    pick(argOne, argTwo); 
+};
+
+start(process.argv[2], process.argv.slice(3).join(""));
